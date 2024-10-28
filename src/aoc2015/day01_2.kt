ba320f9297f14283df7a6f54f9f6c8c740dc5797
harijoshi <hariojoshi@gmail.com>
2024-10-28 14:38:10 +0545

    aoc2015: day01/part2

--- src/aoc2015/day01_2.kt
+++ src/aoc2015/day01_2.kt
@@ -1,5 +1,22 @@
 package aoc2015
 
-fun main(){
+import java.io.File
 
+fun main() {
+    val input = File("src/aoc2015/input01.txt").readText()
+    basementIndexPosition(input)
+}
+
+fun basementIndexPosition(input: String) {
+
+    var sum = 0
+
+    input.forEachIndexed { index, char ->
+        if (char == '(') sum += 1 else sum -= 1
+
+        if (sum == -1){
+            println("Position of Char at which we reach basement =${index + 1}")
+            return
+        }
+    }
 }
